41ca0c4b87d6a7b11eee50659855739f66be8802
Ashani9696 <ashani9996@gmail.com>
2024-03-26 09:17:45 +0530

    Initial Commit

---
+++ SkyBloomShopping/SkyBloomShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyBloomShoppingApp.swift
+//  SkyBloomShopping
+//
+//  Created by Ashani Dilanka on 2024-03-26.
+//
+
+import SwiftUI
+
+@main
+struct SkyBloomShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
